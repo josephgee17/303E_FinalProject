1efdc347d9be388509a370db7c2b0afeab48d539
Joseph Guico <43156960+josephgee17@users.noreply.github.com>
2019-03-10 11:12:50 -0400

    Add files via upload

---
+++ PA1.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello World!")
+    name = input("What is your name? ")
+    print("Hello " + name + "!")
+    print("This program is now complete and will Exit now. Have a great day!")
+
+
+main()
